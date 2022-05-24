c3634993830302e7c6511dabe6e4d90f59e16a86
pedromealhamb <pedro.mealha@mainbrain.pt>
2022-05-24 22:16:04 +0100

    two type Google login > popup and redirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,16 +1,33 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
+import {
+	auth,
+	signInWithGooglePopup,
+	signInWithGoogleRedirect,
+	createUserDocumentFromAuth
+} from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
-	const logGoogleUser = async () => {
+	useEffect(() => {
+		return async function getResponse() {
+			const response = await getRedirectResult(auth);
+			if (response) {
+				const userDocRef = await createUserDocumentFromAuth(response.user);
+			}
+		}
+	}, []);
+
+	const logGooglePopupUser = async () => {
 		const { user } = await signInWithGooglePopup();
 		const userDocRef = await createUserDocumentFromAuth(user);
 	}
+
 	return (
 		<div>
 			<h1>Sign In</h1>
-			<button onClick={ logGoogleUser }>
-				Sign in with Google Popup
-			</button>
+			<button onClick={ logGooglePopupUser }>Sign in with Google Popup</button>
+			<button onClick={ signInWithGoogleRedirect }>Sign in with Google Redirect</button>
 		</div>
 	)
 }
