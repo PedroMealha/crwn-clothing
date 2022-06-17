2f41bf73bfa2a03fd70c6d85895589f37a15ed15
pedromealhamb <pedro.mealha@mainbrain.pt>
2022-06-18 00:49:05 +0100

    added redux and and refacted userContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
 import { Routes, Route } from "react-router-dom";
+
+import {
+	onAuthStateChangedListener,
+	createUserDocumentFromAuth,
+} from './utils/firebase/firebase.utils';
 
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
@@ -6,14 +14,31 @@ import Shop from "./routes/shop/shop.component";
 import Authentication from "./routes/authentication/authentication.component";
 import Checkout from "./routes/checkout/checkout.component";
 
-const App = () =>
-	<Routes>
-		<Route path='/' element={ <Navigation /> }>
-			<Route index element={ <Home /> } />
-			<Route path='shop/*' element={ <Shop /> } />
-			<Route path='auth' element={ <Authentication /> } />
-			<Route path='checkout' element={ <Checkout /> } />
-		</Route>
-	</Routes>
+import { setCurrentUser } from "./store/user/user.action";
+
+const App = () => {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		const unsubscribe = onAuthStateChangedListener((user) => {
+			if (user) {
+				createUserDocumentFromAuth(user);
+			}
+			dispatch(setCurrentUser(user));
+		});
+
+		return unsubscribe;
+	}, [dispatch]);
+
+	return (
+		<Routes>
+			<Route path='/' element={ <Navigation /> }>
+				<Route index element={ <Home /> } />
+				<Route path='shop/*' element={ <Shop /> } />
+				<Route path='auth' element={ <Authentication /> } />
+				<Route path='checkout' element={ <Checkout /> } />
+			</Route>
+		</Routes>)
+}
 
 export default App;
